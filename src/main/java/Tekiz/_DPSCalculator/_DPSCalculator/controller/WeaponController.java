9269345043f65d0c2491141a58fe8dc5b721456b
Callum <callumdavidrossiter@gmail.com>
2024-10-02 12:57:35 +0100

    Updated project to use Spring Security.

--- src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/controller/WeaponController.java
+++ src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/controller/WeaponController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/loadout/weapon")
+//@RequestMapping("api/loadout/weapon")
 public class WeaponController
 {
 	private final LoadoutManager loadoutManager;
