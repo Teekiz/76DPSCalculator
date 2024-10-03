26e3e09b6ea374572b612df45c0beaeb7c799889
Callum <callumdavidrossiter@gmail.com>
2024-10-03 21:39:20 +0100

    Logging for debugging.

--- src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/config/data/RedisConfig.java
+++ src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/config/data/RedisConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
+@EnableRedisHttpSession
 public class RedisConfig {
 
 	@Bean
