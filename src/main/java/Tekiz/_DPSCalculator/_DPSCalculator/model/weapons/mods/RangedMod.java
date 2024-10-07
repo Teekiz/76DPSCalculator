554debbb5e03420cd768e60898bf46c73402015d
Callum <callumdavidrossiter@gmail.com>
2024-10-07 17:53:01 +0100

    Added serialization annotations to RangedMod.java and Receiver.java.

--- src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/model/weapons/mods/RangedMod.java
+++ src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/model/weapons/mods/RangedMod.java
@@ -1,15 +1,19 @@
 package Tekiz._DPSCalculator._DPSCalculator.model.weapons.mods;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 
 /**
  * Represents a generic modification that can be made to a weapon.
  */
-@Data
-@AllArgsConstructor
+@Getter
+@SuperBuilder(toBuilder = true)
 public abstract class RangedMod implements Serializable
 {
-	private String name;
+	@JsonProperty("name")
+	private final String name;
+
 }
