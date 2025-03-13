0fabad67cf779a4dc2ac0b4320a02bfcb8a8796e
Callum Rossiter <59575047+Teekiz@users.noreply.github.com>
2025-03-13 19:47:23 +0000

    Removed expression loader service and added directly into modifier objects.

--- src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/services/context/ModifierExpressionService.java
+++ src/main/java/Tekiz/_DPSCalculator/_DPSCalculator/services/context/ModifierExpressionService.java
@@ -3,11 +3,9 @@ package Tekiz._DPSCalculator._DPSCalculator.services.context;
 import Tekiz._DPSCalculator._DPSCalculator.model.enums.modifiers.ModifierTypes;
 import Tekiz._DPSCalculator._DPSCalculator.model.enums.modifiers.ModifierValue;
 import Tekiz._DPSCalculator._DPSCalculator.model.loadout.Loadout;
-import Tekiz._DPSCalculator._DPSCalculator.services.creation.loading.ExpressionLoaderService;
 import Tekiz._DPSCalculator._DPSCalculator.services.parser.ParsingService;
 import java.io.IOException;
 import java.util.AbstractMap;
-import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,39 +21,36 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ModifierExpressionService
 {
-	private final ParsingService ParsingService;
-	private final HashMap<String, Expression> contextExpressions;
+	private final ParsingService parsingService;
 
 	/**
 	 * The constructor for {@link ModifierExpressionService}.
-	 * @param expressionLoaderService A loader service.
 	 * @param ParsingService A service responsible for parsing and evaluating SpEL (Spring Expression Language) expressions.
 	 * @throws IOException
 	 */
 	@Autowired
-	public ModifierExpressionService(ExpressionLoaderService expressionLoaderService, ParsingService ParsingService) throws IOException
+	public ModifierExpressionService( ParsingService ParsingService) throws IOException
 	{
-		this.ParsingService = ParsingService;
-		this.contextExpressions = expressionLoaderService.getContextInformation();
+		this.parsingService = ParsingService;
 	}
 
 	/**
 	 * A method used to parse and apply a new {@link ModifierTypes} with a determined value.
-	 * @param contextName The name of a modifier that requires additional context.
+	 * @param expressionString The expression of a modifier that requires additional context.
 	 * @param loadout  The loadout that will be used to check against the context expression.
 	 * @return A {@link Map.Entry} of a new {@link ModifierTypes} and a {@link Number} value.
 	 */
-	public Map.Entry<ModifierTypes, ModifierValue<?>> getAdditionalContext(String contextName, Loadout loadout)
+	public Map.Entry<ModifierTypes, ModifierValue<?>> getAdditionalContext(String expressionString, Loadout loadout)
 	{
-		Expression expression = contextExpressions.get(contextName);
-
-		if (expression != null)
+		try
 		{
-			return ParsingService.parseContext(expression, loadout);
+			Expression expression = parsingService.parseString(expressionString);
+			return parsingService.parseContext(expression, loadout);
 		}
-		log.error("Unable to parse expression: {}.", contextName);
-		return new AbstractMap.SimpleEntry<>(ModifierTypes.ERROR_TYPE, new ModifierValue<>(ModifierTypes.ERROR_TYPE, "ERROR"));
+		catch (Exception e)
+		{
+			log.error("Unable to parse expression: {}.", expressionString, e);
+			return new AbstractMap.SimpleEntry<>(ModifierTypes.ERROR_TYPE, new ModifierValue<>(ModifierTypes.ERROR_TYPE, "ERROR"));
+		}
 	}
-
-
 }
